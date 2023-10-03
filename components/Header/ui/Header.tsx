e0df9b0d0837defc21026b78eabaa73aa9f945fd
Egbert HU <62612263+gottaegbert@users.noreply.github.com>
2023-10-03 14:11:10 +0200

    preloader not working ❌

---
+++ components/Header/ui/Header.tsx
@@ -0,0 +1,191 @@
+import {
+  FC,
+  useCallback,
+  useEffect,
+  useState,
+  memo,
+  useContext,
+  useMemo,
+} from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import classNames from 'classnames'
+import { motion } from 'framer-motion'
+
+import useWindowDimensions from '@/hooks/useWindowDimensions'
+import { useScroll } from '@/hooks/useScroll'
+
+import Container from '@/app/layouts/Container'
+import NavigationLayout from '@/components/Header/ui/NavigationLayout/NavigationLayout'
+import LogoTest from '@/ui/Logo/LogoTest'
+import { MobileLayout } from './MobileLayout/MobileLayout'
+
+import { Store } from '@/utils/Store'
+import styles from './Header.module.scss'
+
+type HeaderProps = {
+  theme: 'dark' | 'light'
+}
+
+const inititalHeader = {
+  y: '-100%',
+}
+
+const transition = {
+  duration: 1,
+  ease: 'easeInOut',
+}
+
+const Header: FC<HeaderProps> = ({ theme: initialTheme }) => {
+  const { scrolled, scrollDirection } = useScroll()
+  const [scrolledClassToAdd, setScrolledClassToAdd] = useState(false)
+
+  const [menuOpened, setMenuOpened] = useState(false)
+  const [menuOpenedClass, setMenuOpenedClass] = useState(false)
+
+  const [headerTheme, setHeaderTheme] = useState(initialTheme)
+  const { width } = useWindowDimensions()
+  const router = useRouter()
+  const store = useContext(Store)
+
+  const mods = {
+    [styles[headerTheme]]: true,
+    [styles.scrolled]: scrolledClassToAdd,
+    [styles.opened]: menuOpenedClass,
+  }
+
+  const toggleMenu = () => {
+    setMenuOpened((prev) => !prev)
+  }
+
+  useEffect(() => {
+    if (menuOpened) {
+      setMenuOpenedClass(true)
+    } else {
+      const removeOpenedClass = setTimeout(() => {
+        setMenuOpenedClass(false)
+      }, 1000)
+      return () => clearInterval(removeOpenedClass)
+    }
+  }, [menuOpened])
+
+  const handleScroll = useCallback(() => {
+    if (menuOpened) {
+      return
+    }
+
+    if (window.scrollY < 10) {
+      setScrolledClassToAdd(false)
+      setHeaderTheme(initialTheme)
+    }
+  }, [menuOpened, initialTheme])
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [handleScroll])
+
+  useEffect(() => {
+    if (menuOpenedClass && !scrolled) {
+      setHeaderTheme('dark')
+    }
+
+    if (!menuOpenedClass && !scrolled) {
+      setHeaderTheme(initialTheme)
+    }
+
+    if (menuOpenedClass && scrolled) {
+      setHeaderTheme('dark')
+    }
+
+    if (!menuOpenedClass && scrolled && width <= 991) {
+      setHeaderTheme('light')
+      setScrolledClassToAdd(true)
+    }
+  }, [menuOpenedClass, initialTheme, scrolled])
+
+  useEffect(() => {
+    if (width > 991) setMenuOpened(false)
+  }, [width])
+
+  // useEffect(() => {
+  //   document.body.style.overflow = menuOpened ? 'hidden' : 'auto'
+  // }, [menuOpened])
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setMenuOpened(false)
+    }
+
+    router.events.on('routeChangeStart', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange)
+    }
+  }, [router])
+
+  const headerAnimation = useMemo(() => {
+    return {
+      y:
+        scrollDirection === 'down' || store?.state.isFirstLoading ? '-100%' : 0,
+    }
+  }, [scrollDirection, store?.state.isFirstLoading])
+
+  const handleHeaderAnimationComplete = useCallback(() => {
+    if (scrolled && scrollDirection === 'down' && width > 991) {
+      setScrolledClassToAdd(true)
+      setHeaderTheme('light')
+      return
+    }
+  }, [scrollDirection, scrolled, width])
+
+  const handleLogoClick = useCallback(() => {
+    if (router.pathname !== '/') {
+      setHeaderTheme('light')
+      setScrolledClassToAdd(false)
+    }
+  }, [router.pathname])
+
+  return (
+    <>
+      <motion.header
+        className={classNames(styles['header'], mods)}
+        initial={inititalHeader}
+        animate={headerAnimation}
+        transition={transition}
+        onAnimationComplete={handleHeaderAnimationComplete}
+      >
+        <Container className={styles['header__container']}>
+          <div className={styles['header__content']}>
+            <Link
+              scroll={false}
+              className={styles['header__content_link']}
+              href={'/'}
+              onClick={handleLogoClick}
+            >
+              <div className={styles['logo']}>
+                <LogoTest
+                  className={styles['logo__icon']}
+                  isWhite={headerTheme !== 'light'}
+                  withoutText={scrolledClassToAdd && !menuOpened}
+                />
+              </div>
+            </Link>
+            <NavigationLayout />
+          </div>
+        </Container>
+        <MobileLayout
+          scrolled={scrolled}
+          theme={headerTheme}
+          menuOpened={menuOpened}
+          menuOpenedClass={menuOpenedClass}
+          toggleMenu={toggleMenu}
+        />
+      </motion.header>
+    </>
+  )
+}
+
+export default memo(Header)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,32 @@
 import "../styles/global.scss";
 import type { AppProps } from "next/app";
+import { StoreProvider } from '../utils/Store'
 import { Analytics } from '@vercel/analytics/react';
-
-
+import MainPreloaderWrapper from '../components/MainPreloader/MainPreloaderWrapper'
+import NextNProgress from 'nextjs-progressbar'
+import { AnimatePresence } from 'framer-motion'
+// import AppLayout from '../components/AppLayout'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const onExitComplete = () => {
+    history.scrollRestoration = 'manual'
+    window.scrollTo({ top: 0 })
+  }
   return (
     <>
-      <Component {...pageProps} />
+      {/* <Component {...pageProps} /> */}
+
+      <StoreProvider>
+        {/* <AppLayout> */}
+          <NextNProgress color="#2fbe12" />
+          <div id="portal"></div>
+          <MainPreloaderWrapper />
+          <AnimatePresence onExitComplete={onExitComplete} mode="wait">
+            <Component {...pageProps}/>
+          </AnimatePresence>
+        {/* </AppLayout> */}
+      </StoreProvider>
+
       <Analytics />
     </>
   );
